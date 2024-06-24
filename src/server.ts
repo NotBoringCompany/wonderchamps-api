bd1b68292e0fda2cc2cac2f0f0fdc4cf7a7328cf
suwandre <suwandresukijat@gmail.com>
2024-06-24 19:13:30 +0200

    feat: init server.ts

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config({ path: __dirname + '/../.env' });
+
+const app = express();
+const port = process.env.PORT!;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors()); // temporarily allowing all cors requests
+
+// // memory store for session management
+// app.use(session({
+//     secret: process.env.SESSION_SECRET!,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+app.listen(port, async () => {
+    console.log(`Server running on port ${port}`);
+});
