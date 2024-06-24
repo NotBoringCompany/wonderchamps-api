84e985c6d9853ea2b2d11ff8c613c8ab98bc6450
suwandre <suwandresukijat@gmail.com>
2024-06-24 19:24:32 +0200

    feat: db connections to wonderbits and wonderchamps collections

---
+++ src/utils/db.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/../.env' });
+
+const wonderbitsMongoURI = process.env.WONDERBITS_MONGODB_URI!;
+const wonderchampsMongoURI = process.env.WONDERCHAMPS_MONGODB_URI!;
+
+export const WONDERBITS_CONNECTION = mongoose.createConnection(wonderbitsMongoURI);
+export const WONDERCHAMPS_CONNECTION = mongoose.createConnection(wonderchampsMongoURI);
+
+WONDERBITS_CONNECTION.on('connected', () => {
+    console.log('Connected to Wonderbits database');
+});
+
+WONDERBITS_CONNECTION.on('error', (err) => {
+    console.error(`Error connecting to Wonderbits database: ${err}`);
+});
+
+WONDERCHAMPS_CONNECTION.on('connected', () => {
+    console.log('Connected to Wonderchamps database');
+});
+
+WONDERCHAMPS_CONNECTION.on('error', (err) => {
+    console.error(`Error connecting to Wonderchamps database: ${err}`);
+});
