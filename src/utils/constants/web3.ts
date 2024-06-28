649e1dba20503a7fdb387101198d5c96f7a0dcdc
suwandre <suwandresukijat@gmail.com>
2024-06-28 13:21:54 +0200

    feat: wallet client and account instances for base sepolia

--- src/utils/constants/web3.ts
+++ src/utils/constants/web3.ts
@@ -1,4 +1,5 @@
-import { createPublicClient, http, defineChain } from 'viem';
+import { createPublicClient, http, defineChain, createWalletClient } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
 
 /** Base Sepolia details */
 export const BASE_SEPOLIA = defineChain({
@@ -23,5 +24,19 @@ export const BASE_SEPOLIA = defineChain({
 /** Instantiates a Base Sepolia client instance */
 export const BASE_SEPOLIA_CLIENT = createPublicClient({
     chain: BASE_SEPOLIA,
+    transport: http(),
+    batch: {
+        multicall: true
+    }
+});
+
+/** Instantiates a generic wallet client for Base Sepolia */
+export const WALLET_CLIENT = createWalletClient({
+    chain: BASE_SEPOLIA,
     transport: http()
 });
+
+/** Instantiates the deployer's account */
+export const DEPLOYER_ACCOUNT = privateKeyToAccount(`0x${process.env.DEPLOYER_PRIVATE_KEY!}`);
+/** Instantiates a user's account given their `privateKey` */
+export const USER_ACCOUNT = (privateKey: string) => privateKeyToAccount(`0x${privateKey}`);
