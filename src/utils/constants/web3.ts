997816a199e811291485e3c08f352a9000234957
suwandre <suwandresukijat@gmail.com>
2024-06-28 13:46:58 +0200

    rf: refactored the wallet client and wonderchamps contract

--- src/utils/constants/web3.ts
+++ src/utils/constants/web3.ts
@@ -1,8 +1,10 @@
-import { createPublicClient, http, defineChain, createWalletClient } from 'viem';
+import { createPublicClient, http, defineChain, createWalletClient, getContract, PrivateKeyAccount } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
+import fs from 'fs';
+import path from 'path';
 
 /** Base Sepolia details */
-export const BASE_SEPOLIA = defineChain({
+const BASE_SEPOLIA = defineChain({
     id: 84532,
     name: 'Base Sepolia',
     nativeCurrency: {
@@ -30,13 +32,32 @@ export const BASE_SEPOLIA_CLIENT = createPublicClient({
     }
 });
 
-/** Instantiates a generic wallet client for Base Sepolia */
-export const WALLET_CLIENT = createWalletClient({
+/** Instantiates a wallet client given an `account` for Base Sepolia */
+export const WALLET_CLIENT = (account: PrivateKeyAccount) => createWalletClient({
     chain: BASE_SEPOLIA,
-    transport: http()
+    transport: http(),
+    account
 });
 
 /** Instantiates the deployer's account */
 export const DEPLOYER_ACCOUNT = privateKeyToAccount(`0x${process.env.DEPLOYER_PRIVATE_KEY!}`);
 /** Instantiates a user's account given their `privateKey` */
 export const USER_ACCOUNT = (privateKey: string) => privateKeyToAccount(`0x${privateKey}`);
+
+/** Fetches the Wonderchamps contract's ABI */
+export const WONDERCHAMPS_ABI = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, '../../abi/Wonderchamps.json')
+    ).toString()
+);
+
+
+/** Instantiates the Wonderchamps contract instance */
+export const WONDERCHAMPS_CONTRACT = (account: PrivateKeyAccount) => getContract({
+    address: `0x${process.env.WONDERCHAMPS_CONTRACT_ADDRESS_NO_0x!}`,
+    abi: WONDERCHAMPS_ABI,
+    client: {
+        public: BASE_SEPOLIA_CLIENT,
+        wallet: WALLET_CLIENT(account)
+    }
+});
